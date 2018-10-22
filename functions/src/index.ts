559eaca136d57507381cb80e398a99f784836fee
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-10-22 23:54:13 +0530

    basic express api setup done

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,32 @@
 import * as functions from 'firebase-functions';
+import express = require("express");
+import bodyParser = require("body-parser");
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, GET, PATCH, DELETE, OPTIONS"
+  );
+  next();
 });
+
+app.get('/',(req, res) =>{
+    res.status(200).json({chartreportapi:'works'});
+})
+exports.chartreportapi = functions.https.onRequest(app)
