bb11752d7a8582850d179c30d189a9c5d4750900
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-10-23 01:08:46 +0530

    express file upload package installed

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,11 +2,15 @@ import * as functions from 'firebase-functions';
 import express = require("express");
 import bodyParser = require("body-parser");
 import csvtojson = require('csvtojson');
+// import multer = require('multer');
+import file_upload = require('express-fileupload');
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-
+// const upload = multer();
 
 const app = express();
+app.use(file_upload())
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -30,9 +34,17 @@ app.get('/',(req, res) =>{
     res.status(200).json({chartreportapi:'works'});
 })
 
-app.post('/report', (req, res)=>{
-    let csvData;
-    return csvtojson().fromString(csvData).then(json => {return res.status(201).json({csv:csvData, json:json})})
+// app.post('/report', upload.single(), (req, res)=>{
+//     let csvData = req.file.filename;
+//     console.log(csvData);
+//     // return csvtojson().fromString(csvData).then(json => {return res.status(201).json({csv:csvData, json:json})})
+// })
+
+app.post('/reportfile', (req, res)=>{
+    // let csvData = req.files.csvfile
+    // let csv = (csvData)=>csvData.data.toString('utf8')
+    // console.log(csvData);
+    // return csvtojson().fromString(csvData).then(json => {return res.status(201).json({csv:csvData, json:json})})
 })
 
 exports.chartreportapi = functions.https.onRequest(app)
