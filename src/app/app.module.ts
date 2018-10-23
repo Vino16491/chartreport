9c15e21b2d371fdb8841f577a795ca57f5cc89c0
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-10-23 19:53:28 +0530

    bug fixed for angular forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
-
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatInputModule } from "@angular/material/input";
 import { MatNativeDateModule, MatSortModule } from "@angular/material";
@@ -20,6 +20,7 @@ import { AppService } from "./app.service";
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
