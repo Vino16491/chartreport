77fbeaacc133c1202608485a2b7dcf01a6a74698
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-10-23 19:44:33 +0530

    chart report file updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,190 @@
-import { Component } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import * as csvtojson from 'csvtojson';
+import { Component, OnInit, ViewChild, AfterViewInit } from "@angular/core";
+import { MatTableDataSource, MatSort, MatPaginator } from "@angular/material";
+import { NgForm } from "@angular/forms";
+import * as csvtojson from "csvtojson";
+import {
+  HttpHeaders,
+  HttpClient,
+  HttpResponse,
+  HttpErrorResponse,
+  HttpRequest
+} from "@angular/common/http";
+
+export interface Doctor {
+  Doctor_ID: string;
+  Doctor_Name: string;
+  DOB: string;
+  Employee_Responsible: string;
+  Mobile: string;
+  Address: string;
+  PinCode: string;
+  City: string;
+  District: string;
+  State: string;
+  Country: string;
+  Area_of_Specializations?: string;
+}
+
+export interface Employee {
+  Employee_ID: string;
+  Employee_Name: string;
+  DOB: string;
+  Mobile: string;
+  Address: string;
+  PinCode: string;
+  City: string;
+  District: string;
+  State: string;
+  Country: string;
+  DOJ: string;
+  Designation: string;
+  Manager: string;
+  PAN: string;
+  Adhaar: string;
+}
+
+export interface Product {
+  Product_ID: string;
+  Product_Name: string;
+  Product_Category: string;
+  Unit_Of_Measure: string;
+}
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  customdata;
-  productsdata;
-  employeedata;
-  doctordata;
+  /* For Doctor Date */
+  doctorDisplayedColumns = [
+    "Doctor_ID",
+    "Doctor_Name",
+    "DOB",
+    "Employee_Responsible",
+    "Mobile",
+    "Address",
+    "PinCode",
+    "City",
+    "District",
+    "State",
+    "Country",
+    "Area_of_Specializations"
+  ];
+
+  doctorDataSource = new MatTableDataSource<Doctor>();
+
+  /* For Employee Data */
+  employeeDisplayColumns = [
+    "Employee_ID",
+    "Employee_Name",
+    "DOB",
+    "Mobile",
+    "Address",
+    "PinCode",
+    "City",
+    "District",
+    "State",
+    "Country",
+    "DOJ",
+    "Designation",
+    "Manager",
+    "PAN",
+    "Adhaar"
+  ];
+
+  employeeDataSource = new MatTableDataSource<Employee>();
+
+  /* For product Data */
+  productDisplayColumns = [
+    "Product_ID",
+    "Product_Name",
+    "Product_Category",
+    "Unit_Of_Measure"
+  ];
+  productDataSource = new MatTableDataSource<Product>();
+
+  @ViewChild(MatSort)
+  sort: MatSort;
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
+
   constructor(private http: HttpClient) {}
+  ngOnInit() {}
 
-  fileupload(event) {
-    let file: File = event.target.files[0]
-    let fileReader : FileReader = new FileReader();
-    fileReader.readAsText(file)
-    // console.log(fileReader.result)
-    fileReader.onload = (ev)=>{
-      let csvdata = fileReader.result.toString()
-      return csvtojson().fromString(csvdata).then(json=>{
+  ngAfterViewInit() {
+    /* for doctor */
+    this.doctorDataSource.sort = this.sort;
+    this.doctorDataSource.paginator = this.paginator;
 
-        console.log(JSON.stringify(json))
-      
-    })}
-    
+    /* for employee */
+    this.employeeDataSource.sort = this.sort;
+    this.employeeDataSource.paginator = this.paginator;
+
+    /* for product */
+    this.productDataSource.sort = this.sort;
+    this.productDataSource.paginator = this.paginator;
+  }
+
+  doFilter(filterValue: string) {
+    this.doctorDataSource.filter = filterValue.trim().toLowerCase();
+    this.employeeDataSource.filter = filterValue.trim().toLowerCase();
+    this.productDataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  filepost(files: FileList) {
+    if (files && files.length > 0) {
+      let file: File = files.item(0);
+      let fileReader: FileReader = new FileReader();
+      fileReader.readAsText(file);
+      // console.log(fileReader.result)
+      fileReader.onload = ev => {
+        let csvdata = fileReader.result.toString();
+        return csvtojson()
+          .fromString(csvdata)
+          .then(json => {
+            console.log(file.name);
+
+            this.doctorDataSource.data = json;
+          });
+      };
+    }
+  }
+
+  employeefilepost(files: FileList) {
+    if (files && files.length > 0) {
+      let file: File = files.item(0);
+      let fileReader: FileReader = new FileReader();
+      fileReader.readAsText(file);
+      // console.log(fileReader.result)
+      fileReader.onload = ev => {
+        let csvdata = fileReader.result.toString();
+        return csvtojson()
+          .fromString(csvdata)
+          .then(json => {
+            console.log(file.name);
+
+            this.employeeDataSource.data = json;
+          });
+      };
+    }
+  }
+
+  productfilepost(files: FileList) {
+    if (files && files.length > 0) {
+      let file: File = files.item(0);
+      let fileReader: FileReader = new FileReader();
+      fileReader.readAsText(file);
+      // console.log(fileReader.result)
+      fileReader.onload = ev => {
+        let csvdata = fileReader.result.toString();
+        return csvtojson()
+          .fromString(csvdata)
+          .then(json => {
+            console.log(file.name);
+
+            this.productDataSource.data = json;
+          });
+      };
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatInputModule } from "@angular/material/input";
+import { MatNativeDateModule, MatSortModule } from "@angular/material";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSelectModule } from "@angular/material/select";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { AppService } from "./app.service";
 
 @NgModule({
@@ -16,7 +23,17 @@ import { AppService } from "./app.service";
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatInputModule
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatSortModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    MatProgressSpinnerModule
   ],
   providers: [AppService],
   bootstrap: [AppComponent]
